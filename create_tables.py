c8d13f3344da54625c101a096328ed28b1d4ff49
Daniel Tavares <daniel.mts@gmail.com>
2019-07-31 19:11:02 +0100

    use db.get_connection on create_tables

--- create_tables.py
+++ create_tables.py
@@ -1,25 +1,20 @@
-import psycopg2
+#!/usr/bin/env python3
+
+from db import get_connection
 from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database():
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
-    conn.set_session(autocommit=True)
-    cur = conn.cursor()
-    
+    conn, cur = get_connection(db="template1")
+
     # create sparkify database with UTF8 encoding
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
     cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
 
     # close connection to default database
-    conn.close()    
-    
-    # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
-    cur = conn.cursor()
-    
-    return cur, conn
+    cur.close()
+    conn.close()
 
 
 def drop_tables(cur, conn):
@@ -29,17 +24,22 @@ def drop_tables(cur, conn):
 
 
 def create_tables(cur, conn):
+    conn, cur = get_connection()
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    cur, conn = create_database()
-    
+    create_database()
+
+    conn, cur = get_connection()
+
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
+    # close connection to database
+    cur.close()
     conn.close()
 
 
